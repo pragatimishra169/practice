6a9b975f40835e1f6b9c030ecdc6a164f21b59bd
pragatimishra169 <48344428+pragatimishra169@users.noreply.github.com>
2019-03-12 18:33:27 +0530

    Add files via upload

---
+++ slideshow.js
@@ -0,0 +1,16 @@
+
+
+
+
+function carousel() {
+  var myIndex = 0;
+  var i;
+  var x = document.getElementsByClassName("slide_img");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
